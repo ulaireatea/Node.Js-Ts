63a92384fe2bcaf92c176e2cde78b533a184df8e
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2021-02-04 14:16:57 -0300

    dificil isso hein

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '98406d1a8fbd57129d6b2ffb7c0a086a',
+        expiresIn: '1d'
+    }
+}
---
+++ src/routes/locations.routes.ts
@@ -0,0 +1,139 @@
+import { Router } from 'express';
+import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
+import knex from '../database/connection';
+import multerConfig from '../config/multer';
+import isAuthenticated from '../middlewares/isAuthenticated';
+
+const locationsRouter = Router();
+
+const upload = multer(multerConfig);
+
+locationsRouter.use(isAuthenticated);
+
+locationsRouter.get('/', async (request, response) => {
+    const { city, uf, items } = request.query;
+
+    if (city && uf && items) {
+        const parsedItems: Number[] = String(items).split(',').map(item => Number(item.trim()));
+
+        const locations = await knex('locations')
+            .join('location_items', 'locations.id', '=', 'location_items.location_id')
+            .whereIn('location_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('locations.*');
+
+            return response.json(locations);
+    } else {
+        const locations =  await knex('locations').select('*');
+
+        return response.json(locations);
+    }
+});
+
+locationsRouter.get('/:id', async (request, response) => {
+    const { id } = request.params;
+
+    const location = await knex('locations').where('id', id).first();
+
+    if (!location) {
+        return response.status(400).json({ message: 'Location not found.'});
+    }
+
+    const items = await knex('items')
+        .join('location_items', 'items.id', '=', 'location_items.item_id')
+        .where('location_items.location_id', id)
+        .select('items.title')
+
+    return response.json({ location, items});
+});
+
+locationsRouter.post('/', celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+    })
+}, {
+    abortEarly: false
+}), async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const location = {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    };
+
+    const transaction = await knex.transaction();
+
+    const newIds = await transaction('locations').insert(location);
+
+    const location_id = newIds[0];
+
+    const locationItems = items.map(async (item_id: number) => {
+        const selectedItem = await transaction('items').where('id', item_id).first();
+
+        if (!selectedItem) {
+            return response.status(400).json({ message: 'Item not found.'});
+        }
+        return {
+            item_id,
+            location_id
+        }
+    });
+
+    console.log(locationItems);
+
+    await transaction('location_items').insert(locationItems);
+
+    await transaction.commit();
+
+    return response.json({
+        id: location_id,
+        ...location
+    });
+});
+
+locationsRouter.put('/:id', upload.single('image') , async (request, response) => {
+    const { id } = request.params;
+
+    const image = request.file.filename;
+
+    const location = await knex('locations').where('id', id).first();
+
+    if (!location) {
+        return response.status(400).json({ message: 'Location not found.' });
+    }
+
+    const locationUpdate = {
+        ...location,
+        image
+    };
+
+    await knex('locations').update(locationUpdate).where('id', id);
+
+    return response.json(locationUpdate);
+});
+
+export default locationsRouter;
